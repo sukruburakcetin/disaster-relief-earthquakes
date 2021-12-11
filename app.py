f21ea4974064a33ba15156bb61f518ed50979c26
sukruburakcetin <cetin.sukruburak@gmail.com>
2021-12-11 20:30:59 +0300

    fixes

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import requests
 import pandas as pd
 from bs4 import BeautifulSoup
-from flask import Flask, render_template, request, flash
+from flask import Flask, render_template, request, flash, jsonify
 
 app = Flask(__name__)
 app.secret_key = "imm_sukruburakcetin"
@@ -47,15 +47,18 @@ df["Buyukluk"] = df["Buyukluk"].astype("float64")
 
 df_p = df.T.to_dict('dict')
 
+
 @app.route("/hello")
 def index():
-	flash("what's your name?")
-	return render_template("index.html")
+    flash("what's your name?")
+    return render_template("index.html")
+
 
 @app.route("/greet", methods=['POST', 'GET'])
 def greeter():
-	flash("Hi " + str(request.form['name_input']) + ", great to see you!")
-	return render_template("index.html")
+    flash("Hi " + str(request.form['name_input']) + ", great to see you!")
+    return render_template("index.html")
+
 
 # A route to return all of the available entries in our catalog.
 @app.route('/api/v1/resources/earthquakes/all', methods=['GET'])
